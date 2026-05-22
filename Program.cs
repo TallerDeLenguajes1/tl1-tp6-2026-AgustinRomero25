57ff4c8ccd91ae42216e2fbe145f569e111220b2
Agustin Romero <agustin.romero.31.7.00@gmail.com>
2026-05-22 03:09:44 -0300

    Calculadora V2 terminada

--- Program.cs
+++ Program.cs
@@ -1,29 +1,103 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Linq.Expressions;
+using System.Globalization;
 
-int a;
-int b;
-a = 10;
-b = a;
+Console.WriteLine("------------------- EJERCICIO 3 -----------------------\n");
+Console.WriteLine("------------------- CALCULADORA V2 -----------------------\n");
 
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
+string s1;
+string s2;
+double num1;
+double num2;
+int eleccion;
+bool SecCheck;
+double resultado;
 
-Console.WriteLine("------------------- EJERCICIO 1 ----------------\n");
-Console.WriteLine("Ingrese un numero mayor a 0: ");
-string entry = Console.ReadLine();
-int num1;
-bool stringaint = int.TryParse(entry, out num1);
-Console.WriteLine($"El numero ingresado es: {num1}");
-
-if (num1 > 0)
+do
 {
-    int resto = 0;
-    int invertido = 0;
-    while (num1 != 0)
+    Console.WriteLine("Ingrese el numero a operar: ");
+    s1 = Console.ReadLine();
+    SecCheck = double.TryParse(s1, CultureInfo.InvariantCulture, out num1);
+    if(!SecCheck)
     {
-        resto = num1 % 10;
-        invertido = (invertido * 10) + resto;
-        num1 = num1 / 10;
-    }
-    Console.WriteLine($"El numero invertido es: {invertido}");
+        Console.WriteLine("Eso no es un numero. \nPor favor, ingrese un numero valido.\n");
+    } 
+} while (!SecCheck);
+
+do
+{
+    Console.WriteLine("¿Qué operacion desea realizar en el número?\n1.valor absoluto\n2.cuadrado\n3.raíz cuadrada\n4.seno\n5.coseno\n6.la parte entera de un tipo float\n");
+    s2 = Console.ReadLine();
+    SecCheck = int.TryParse(s2, out eleccion) && eleccion >= 1 && eleccion <= 6;
+    if(!SecCheck)
+        {
+            Console.WriteLine("Opcion no valida. \nPor favor, ingrese un numero del 1 al 6.\n");
+        }
+} while(!SecCheck);
+
+
+switch (eleccion)
+{
+    case 1:
+        resultado = Math.Abs(num1);
+        Console.WriteLine($"----------------- Resultado: {resultado} --------------------\n"); 
+    break;
+
+    case 2:
+        resultado = num1 * num1;
+        Console.WriteLine($"----------------- Resultado: {resultado} --------------------\n");
+    break;
+
+    case 3:
+        if(num1 >= 0)
+        {
+            resultado = Math.Sqrt(num1);
+            Console.WriteLine($"----------------- Resultado: {resultado} --------------------\n");
+        } else
+        {
+            Console.WriteLine("No se permiten raices negativas. Intente solo con numeros positivos.");
+        }
+    break;
+
+    case 4:
+    resultado = Math.Sin(num1);
+    Console.WriteLine($"----------------- Resultado: {resultado} --------------------\n");
+    break;
+
+    case 5:
+    resultado = Math.Cos(num1);
+    Console.WriteLine($"----------------- Resultado: {resultado} --------------------\n");
+    break;
+
+    case 6:
+    int resultadoEntero = (int)num1;
+    Console.WriteLine($"----------------- Resultado: {resultadoEntero} --------------------\n");
+    break;
 }
+
+Console.WriteLine("------------------- ¿CUAL ES EL MAYOR Y CUAL EL MENOR? -----------------------\n");
+
+do
+{
+    Console.WriteLine("Ingrese el primer numero: ");
+    s1 = Console.ReadLine();
+    SecCheck = double.TryParse(s1, CultureInfo.InvariantCulture, out num1);
+    if(!SecCheck)
+    {
+        Console.WriteLine("Eso no es un numero. \nPor favor, ingrese un numero valido.\n");
+    } 
+} while (!SecCheck);
+
+do
+{
+    Console.WriteLine("Ingrese el segundo numero: ");
+    s2 = Console.ReadLine();
+    SecCheck = double.TryParse(s2, CultureInfo.InvariantCulture, out num2);
+    if(!SecCheck)
+    {
+        Console.WriteLine("Eso no es un numero. \nPor favor, ingrese un numero valido.\n");
+    } 
+} while (!SecCheck);
+
+double mayor = Math.Max(num1, num2);
+double menor = Math.Min(num1, num2);
+Console.WriteLine($"El numero mayor es: {mayor}\nEl numero menor es: {menor}");
